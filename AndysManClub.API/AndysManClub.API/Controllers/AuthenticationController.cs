00000a2eb6110aa230eef07f4ac68f8a5c56d5ec
Dan du Preez <dandupreez@gmail.com>
2024-02-09 12:35:27 +0000

    Initial API commit

---
+++ AndysManClub.API/AndysManClub.API/Controllers/AuthenticationController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AndysManClub.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthenticationController : ControllerBase
+{
+    [HttpGet]
+    public ActionResult Get(string name)
+    {
+        return Ok($"Hello {name}");
+    }
+}
