13d94e59be1c16b0115b3ef38e88e8bce7671106
Yousuf <yousufbhayat@hotmail.co.uk>
2024-02-29 20:10:51 +0000

    New UI project

---
+++ AndysManClub.API/AndysManClub.UI/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AndysManClub.UI
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AndysManClub.API/AndysManClub.UI/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace AndysManClub.UI
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
