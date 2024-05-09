5c9b4e4bddc59d4ccea12f8f75e144d4141d49f2
Yousuf <yousufbhayat@hotmail.co.uk>
2024-05-09 21:01:30 +0100

    The entities are not perfect

--- AndysManClub.API/AndysManClub.Data/AMCContext.cs
+++ AndysManClub.API/AndysManClub.Data/AMCContext.cs
@@ -12,12 +12,34 @@ namespace AndysManClub.Data
         {
         }
         
-        public virtual DbSet<PersonEvent> People { get; set; }
+        public virtual DbSet<EventVolunteers> People { get; set; }
         
         public virtual DbSet<AmcEvent> Events { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
+            builder.Entity<Person>(entity =>
+            {
+            });
+
+            builder.Entity<AmcEvent>(entity =>
+            {
+            });
+
+            builder.Entity<PersonEvent>(entity =>
+            {
+                entity.HasOne(x => x.Event)
+                    .WithMany(x => x.Volunteers)
+                    .HasForeignKey(x => x.Event)
+                    .HasConstraintName("FK_PersonEvent_AmcEvent_AmcEvent");
+
+                entity.HasOne(x => x.Volunteer)
+                    .WithMany(x => x.Volunteers)
+                    .HasForeignKey(x => x.Volunteer)
+                    .HasConstraintName("FK_PersonEvent_Person_Person");
+
+            });
+
             base.OnModelCreating(builder);
             // Customize the ASP.NET Core Identity model and override the defaults if needed.
             // For example, you can rename the ASP.NET Core Identity table names and more.
--- AndysManClub.API/AndysManClub.Domain/DTO/Person.cs
+++ AndysManClub.API/AndysManClub.Domain/DTO/Person.cs
@@ -2,6 +2,10 @@ namespace AndysManClub.Domain.DTO;
 
 public class Person
 {
+    public Guid Id { get; set; }
+
     public required string FirstName { get; set; }
     public required string LastName { get; set; }
+    public List<PersonEvent> Volunteers { get; set; } // better name?
+
 }
